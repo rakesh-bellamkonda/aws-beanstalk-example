eabf89b37a348a4466440d653e4e3b512174daf4
unknown <Rakesh@rakesh.example.com>
2020-12-15 17:06:07 -0600

    add project

---
+++ MavenWebProject/src/main/java/com/helloworld/test/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.helloworld.test;
+ import java.io.IOException;
+ import javax.servlet.ServletException;
+ import javax.servlet.annotation.WebServlet;
+ import javax.servlet.http.HttpServlet;
+ import javax.servlet.http.HttpServletRequest;
+ import javax.servlet.http.HttpServletResponse;
+ @WebServlet("/helloworld")
+ public class HelloWorld extends HttpServlet{
+	 
+	@Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    resp.setContentType("text/plain");
+    resp.getWriter().write("SP Coding Test : Hello World");
+    }
+ }
